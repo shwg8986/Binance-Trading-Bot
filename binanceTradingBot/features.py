d9c84751414a9974f6b5978a2723da2040ed2f9e
shwg <67375194+shwg8986@users.noreply.github.com>
2023-02-20 23:17:10 +0900

    Update features.py

--- binanceTradingBot/features.py
+++ binanceTradingBot/features.py
@@ -15,11 +15,9 @@ def calc_features(df):
     orig_columns = df.columns
     hilo = (df['hi'] + df['lo']) / 2
 
-
     # MANAUSDT用の特徴量
     df['diff'] = (close - close.shift(1)) / close * 100
-    df['ema_diff3'] = talib.EMA(df['diff'], timeperiod=10) #直前の値動きのEMA
-    df['sig3'] = df['ema_diff3'] - df['ema_diff3'].shift(16) #直前の値動きのEMAと過去の差分
-
+    df['ema_diff3'] = talib.EMA(df['diff'], timeperiod=10)
+    df['sig3'] = df['ema_diff3'] - df['ema_diff3'].shift(16)
 
     return df
